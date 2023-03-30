7fab1832ef43adc66993111fbd958e90c3f38d58
Malik Iftikhar <iftikhar@mytrackee.com>
2023-03-30 17:23:21 +0500

    login process

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -60,7 +60,7 @@
 									</p>
 								</div> -->
 			      	</div>
-							<form action="/login_user" method="POST" class="signin-form">
+							<form action="/~2013464/fitness/public/login_user" method="POST" class="signin-form">
 								@csrf
 			      		<div class="form-group mb-3">
 			      			<label class="label" for="name">Email</label>
